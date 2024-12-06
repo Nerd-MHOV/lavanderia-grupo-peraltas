1b62aa5ed428c4f17d14f9d310ae86d30d11b8ea
Matheus Henrique <84547045+Nerd-MHOV@users.noreply.github.com>
2024-12-06 15:54:05 -0300

    singleton prisma instance

--- src/core/db/db.ts
+++ src/core/db/db.ts
@@ -12,22 +12,31 @@ import dbInput from './inputs.db';
 import dbDepartment from './department.db';
 import dbReturn from './return.db';
 
-const prismaClient = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+
+
 
 const db = ({
-    user: dbUser(prismaClient),
-    collaborator: dbCollaborator(prismaClient),
-    department: dbDepartment(prismaClient),
-    product:  dbProduct(prismaClient),
-    barcode: dbBarcodes(prismaClient),
-    inventory: dbInventory(prismaClient),
-    input: dbInput(prismaClient),
-    output: dbOutput(prismaClient),
-    outputOrder: dbOutputOrder(prismaClient),
-    return: dbReturn(prismaClient),
-    service: dbService(prismaClient),
-    type: dbType(prismaClient),
-    $transaction: prismaClient.$transaction,
+    user: dbUser(prisma),
+    collaborator: dbCollaborator(prisma),
+    department: dbDepartment(prisma),
+    product:  dbProduct(prisma),
+    barcode: dbBarcodes(prisma),
+    inventory: dbInventory(prisma),
+    input: dbInput(prisma),
+    output: dbOutput(prisma),
+    outputOrder: dbOutputOrder(prisma),
+    return: dbReturn(prisma),
+    service: dbService(prisma),
+    type: dbType(prisma),
+    $transaction: prisma.$transaction,
 });
 
 export default db;
