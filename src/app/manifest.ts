8bc92703690202f3ad1fa77bdc70b347a17003cf
Matheus Henrique <84547045+Nerd-MHOV@users.noreply.github.com>
2025-03-21 15:28:23 -0300

    new image to manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -6,14 +6,19 @@ export default function manifest(): MetadataRoute.Manifest {
     short_name: "Lavanderia",
     description: "Lavanderia",
     start_url: "/",
-    display: "fullscreen",
+    display: "standalone",
     background_color: "#fff",
     theme_color: "#fff",
     icons: [
       {
         src: "/favicon.ico",
-        sizes: "512x512",
+        sizes: "192x192",
         type: "image/x-icon",
+      },
+      {
+        src: "/imagens/GP.png",
+        sizes: "512x512",
+        type: "image/png",
       },
     ],
   };
